ac0b3ca24f76b6fb20cf2ac4056f2fe5c1a7e784
Zunair <zunairkhurram@gmail.com>
2025-01-22 17:01:55 +1100

    faceting for close/distant datasets

--- R/snp_hist.R
+++ R/snp_hist.R
@@ -12,37 +12,24 @@ library(ggplot2)
 #' @return a histogram plot of snp distances using ggplot
 #'
 #' @export
-snp_hist <- function(trans_snp_dist, unrelated_snp_dist=NULL, snp_threshold=NULL, limits=c(NA,100), title="SNP Distance Histogram", separate_colour=FALSE){
-  if (separate_colour==TRUE){
+snp_hist <- function(trans_snp_dist, unrelated_snp_dist=NULL, snp_threshold=NULL, limits=c(NA,100), title="SNP Distance Histogram"){
     # Combine the datasets into a data frame
     data <- data.frame(
       SNP_Distance = c(unrelated_snp_dist, trans_snp_dist),
       Dataset = c(rep("Unrelated", length(unrelated_snp_dist)),rep("Mixed", length(trans_snp_dist)))
-
     )
+    data$Dataset <- as.factor(data$Dataset)
 
     # Create the plot
-    ggplot(data, aes(x = SNP_Distance, fill = Dataset)) +
+    ggplot(data, aes(x = SNP_Distance)) +
       scale_x_continuous(limits = limits) +
-      geom_histogram(data = subset(data, Dataset == "Unrelated"),
-                     binwidth = 1, position = "identity", alpha = 0.6) +
-      geom_histogram(data = subset(data, Dataset == "Mixed"),
-                     binwidth = 1, position = "identity", alpha = 0.8) +
-      scale_fill_manual(values = c("Mixed" = "darkseagreen4", "Unrelated" = "red3")) +
-      geom_vline(xintercept = snp_threshold + 0.5, color = "red3",alpha=0.7, linetype = "dashed") +
+      geom_histogram(binwidth = 1, position = "identity") +
+      geom_vline(xintercept = snp_threshold + 0.5, color = "red3", alpha = 0.7, linetype = "dashed") +
+      facet_wrap(~Dataset, ncol=1) +
       labs(
         title = title,
         x = "SNP Distances",
-        y = "Count",
-        fill = "Dataset"
+        y = "Count"
       ) +
       theme_minimal()
-    } else{
-  ggplot(data=data.frame(x=append(trans_snp_dist, unrelated_snp_dist)), aes(x))+
-  scale_x_continuous(limits = limits)+
-  geom_histogram(binwidth = 1)+
-  geom_vline(xintercept=snp_threshold+0.5, color="red3",alpha=0.7, linetype="dashed")+
-  labs(title = title, x="SNP Distances", y="Count")+
-  theme_minimal()
-  }
-}
+    }
