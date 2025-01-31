942ace91a2ed96bf731f1a94c20e80d569017b88
Zunair <zunairkhurram@gmail.com>
2025-01-31 17:12:48 +1100

    ensures consistent colours for mixed/distant datasets

--- R/threshold_comparison_plot.R
+++ R/threshold_comparison_plot.R
@@ -7,7 +7,7 @@
 #' @param identity whether to include an identity threshold as well
 #' @param percent_dist_mixed percent distances from a mixed transmission data set, needed if identity thresholds are considered
 #' @param percent_dist_distant percent distances from a distant transmission data set, needed if identity thresholds are considered
-#' @param identity_thresh identity threshold to use
+#' @param identity_thresh identity threshold to use (as percentage)
 #' @param k_est proportion related line to plot
 #' @param title title of plot
 #' @param labels whether to include labels on the plot
@@ -61,13 +61,14 @@ threshold_comparison_plot <- function(trans_snp_dist, unrelated_snp_dist, snp_th
       dataset = recode(dataset, mixed = dataset_names[1], distant = dataset_names[2]),
 
     )|>
+    mutate(dataset=factor(dataset, levels=dataset_names))|>
     mutate(label = paste0(n, "/", tot, " (", round(p*100, 2), "%)"))
 
  #return(data)
 
   ggplot(data, aes(x = p, y = method, fill = dataset)) +
     geom_bar(stat = "identity", position = "dodge", width=0.9) +
-    scale_fill_manual(values = c(  "darkseagreen", "red3")) +
+    scale_fill_manual(values = c( "darkseagreen", "red3")) +
     scale_x_continuous(limits=c(0,1), expand = c(0,0))+
     geom_vline(xintercept=k_est, linetype="solid", colour="grey20", alpha=0.6)+
     geom_text(aes(label = if (labels) label else NA, x = 0.5),
