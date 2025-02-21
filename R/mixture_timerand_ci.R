c52ce5fbfc172d2e10daa3fff846e53d41286967
Zunair <zunairkhurram@gmail.com>
2025-02-21 17:08:11 +1100

    correctly implemented truncation point

--- R/mixture_timerand_ci.R
+++ R/mixture_timerand_ci.R
@@ -15,15 +15,15 @@
 #' @export
 #'
 #' @examples
-mixture_timerand_ci <- function(trans_snp_dist, unrelated_snp_dist, trans_time_dist=NA, trans_sites=NA,
+mixture_timerand_ci <- function(trans_snp_dist, unrelated_snp_dist, trans_time_dist=NA, trans_sites=NA, truncation_point=NA,
                                   sample_size=length(trans_snp_dist), sample_n=500, permutations=3, confidence_level=0.95,
                                   within_individual=FALSE, subjectA_id, subjectB_id,
                                   start_params=NA, ci_data=NA,  raw=FALSE, title=NULL){
   #unadjusted result
   normal_data <- tibble(snp_dist=trans_snp_dist, time_dist=trans_time_dist, sites=trans_sites)
-  normal_result <- mixture_snp_cutoff(normal_data$snp_dist,unrelated_snp_dist, normal_data$time_dist,normal_data$sites)
+  normal_result <- mixture_snp_cutoff(normal_data$snp_dist,unrelated_snp_dist, normal_data$time_dist,normal_data$sites, truncation_point=truncation_point)
   if(anyNA(ci_data)){
-    normal_ci <- mixture_snp_cutoff_ci(normal_data$snp_dist,unrelated_snp_dist, normal_data$time_dist,normal_data$sites,
+    normal_ci <- mixture_snp_cutoff_ci(normal_data$snp_dist,unrelated_snp_dist, normal_data$time_dist,normal_data$sites, truncation_point=truncation_point,
                                        sample_size=sample_size, sample_n=sample_n, confidence_level=confidence_level,
                                        start_params = c(normal_result[3], normal_result[2]))
   } else{
@@ -56,9 +56,9 @@ mixture_timerand_ci <- function(trans_snp_dist, unrelated_snp_dist, trans_time_d
 
 
   timerand_data <- tibble(snp_dist=trans_snp_dist, time_dist=sample(trans_time_dist, length(trans_time_dist)), sites=trans_sites)
-  timerand_result <- mixture_snp_cutoff(timerand_data$snp_dist,unrelated_snp_dist, timerand_data$time_dist,timerand_data$sites)
+  timerand_result <- mixture_snp_cutoff(timerand_data$snp_dist,unrelated_snp_dist, timerand_data$time_dist,timerand_data$sites, truncation_point=truncation_point)
   timerand_ci <- mixture_snp_cutoff_ci(timerand_data$snp_dist,unrelated_snp_dist, timerand_data$time_dist,timerand_data$sites,
-                                       sample_size=sample_size, sample_n=sample_n, confidence_level=confidence_level,
+                                       sample_size=sample_size, sample_n=sample_n, confidence_level=confidence_level, truncation_point=truncation_point,
                                        start_params = c(normal_result[3], normal_result[2]))
   # Append results to 'result'
   result <- bind_rows(result, tibble(
@@ -81,14 +81,23 @@ mixture_timerand_ci <- function(trans_snp_dist, unrelated_snp_dist, trans_time_d
       scale_y_continuous(#transform = "log10"
                          )+
       labs(title=title,
-           x = NA,
+           x = NULL,
            y = "Rate") +
       theme_minimal()
+
+    outcome <- tibble(
+      n_permutations=permutations,
+      n_overlapping_est=sum(result$`95%`[result$method!="Normal"]>=result$point_est[1]),
+      perc_overlapping_est=sum(result$`95%`[result$method!="Normal"]>=result$point_est[1])/length(result$`95%`[result$method!="Normal"]),
+      n_overlapping_lowci=sum(result$`95%`[result$method!="Normal"]>=result$`5%`[1]),
+      perc_overlapping_lowci=sum(result$`95%`[result$method!="Normal"]>=result$`5%`[1])/length(result$`95%`[result$method!="Normal"])
+    )
 
 
   xres_timerand <- list(
     result=result,
-    rawtimerand=rawtimerand,
+    raw_results=rawtimerand,
+    outcome=outcome,
     plot=plot
   )
 }
