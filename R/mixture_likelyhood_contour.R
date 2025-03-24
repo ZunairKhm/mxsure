81b4a453428dbc7603f995a29ed180f7a95fcf78
Zunair <zunairkhurram@gmail.com>
2025-03-24 16:52:09 +1100

    Updated contour plots to consider SNP truncations

--- R/mixture_likelyhood_contour.R
+++ R/mixture_likelyhood_contour.R
@@ -9,15 +9,25 @@ log_sum_exp <- function(log_a, log_b) {
   return(log_a + log(1 + exp(log_b - log_a)))
 }
 
-negllk <- function(k, lambda, x, t, s, nbfitmu, nbfitsize){
+negllk <- function(k, lambda, x, t, s, nbfitmu, nbfitsize,truncation_point, prior_k, prior_lambda){
 
   -log(-sum(pmap_dbl(list(x, t,s), ~ {log_sum_exp(log(k) + dpois(x = ..1,
-                                                           lambda =  lambda*..2*(..3/1000000), #gives rate esimate per day time per million bp
-                                                           log = TRUE),
-                                            log(1-k) + dnbinom(x = ..1,
-                                                               size = nbfitsize,
-                                                               mu = nbfitmu,
-                                                               log = TRUE))})))
+                                                                 lambda =  lambda*..2*(..3/1000000), #gives rate esimate per day time per million bp
+                                                                 log = TRUE) -
+                                                    ppois(truncation_point,
+                                                          lambda =  lambda*..2*(..3/1000000),
+                                                          log = TRUE ),
+                                                  log(1-k) + dnbinom(x = ..1,
+                                                                     size = nbfitsize,
+                                                                     mu = nbfitmu,
+                                                                     log = TRUE)-
+                                                    pnbinom(truncation_point,
+                                                            size = nbfitsize,
+                                                            mu = nbfitmu,
+                                                            log = TRUE))+
+      ifelse(!anyNA(prior_k),  dbeta(k, prior_k[1], prior_k[2], log = TRUE), 0)+
+      ifelse(!anyNA(prior_lambda), dgamma(lambda, prior_lambda[1], prior_lambda[2], log = TRUE),0)
+  })))
 }
 
 #' Contour plot for likelyhood estimation of mixture distribution mutation rate estimation
@@ -37,12 +47,52 @@ negllk <- function(k, lambda, x, t, s, nbfitmu, nbfitsize){
 #'
 #' @examples
 #'
-mixture_likelyhood_contour <- function(trans_snp_dist, unrelated_snp_dist, trans_time_dist, trans_sites,
+mixture_likelyhood_contour <- function(trans_snp_dist, unrelated_snp_dist, trans_time_dist, trans_sites, threshold=NA, truncation_point=NA,  prior_lambda=NA, prior_k=NA,
                                        resolution=100, lambda_limits=c(0.0001, 1), k_limits=c(0.2,0.99), estimate=NULL, low_ci=NULL, high_ci=NULL,
                                        bins=NULL, title="Mixture Likelihood Contour Plot"){
 
-  nb_fit <- suppressWarnings( MASS::fitdistr(x=unrelated_snp_dist, densfun = "negative binomial"
-  ))
+  #defining distribution functions for the truncated negative binomial distr
+  dtruncnbinom <<- function(x, mu, size){
+    dnbinom(x = x, size = size, mu = mu) / pnbinom(truncation_point, size = size, mu = mu)
+  }
+  ptruncnbinom <<- function(q, mu, size){
+    pnbinom(q = q, size = size, mu = mu) / pnbinom(truncation_point, size = size, mu = mu)
+  }
+  qtruncnbinom <<- function(p, mu, size){
+    qnbinom(p = p*(pnbinom(truncation_point, size=size, mu=mu)), size = size, mu = mu)
+  }
+
+  #truncating data
+  if(is.na(truncation_point)){
+    truncation_point <- Inf
+  }
+  unrelated_snp_dist_orig <- unrelated_snp_dist
+  x <- tibble(trans_snp_dist, trans_time_dist, trans_sites)
+  x <- filter(x, trans_snp_dist<truncation_point)
+  trans_snp_dist <- x$trans_snp_dist
+  trans_time_dist <- x$trans_time_dist
+  trans_sites <- x$trans_sites
+  unrelated_snp_dist <- unrelated_snp_dist[unrelated_snp_dist<truncation_point]
+
+  #setting priors
+  if(length(prior_lambda)==1){
+    if(is.element(prior_lambda, "default")){
+      prior_lambda <- c(1.06, 0.44)
+    }}
+  if(length(prior_k)==1){
+    if(is.element(prior_k, "default")){
+      prior_k <- c(1.60, 1.25)
+    }}
+
+  # distant dataset fitting
+  m <- mean(unrelated_snp_dist)
+  v <- var(unrelated_snp_dist)
+  size <- if (v > m) {
+    m^2/(v - m)
+  }else{100}
+
+  nb_fit <- fitdistrplus::fitdist(unrelated_snp_dist, dist="truncnbinom", start=list(mu=m, size=size))
+
   lambda <- exp(seq(log(lambda_limits[1]),log(lambda_limits[2]), length.out = resolution))
   k=seq(k_limits[1],k_limits[2],length.out=resolution)
 
@@ -54,13 +104,16 @@ mixture_likelyhood_contour <- function(trans_snp_dist, unrelated_snp_dist, trans
     k_val <- row["k"]
     lambda_val <- row["lambda"]
 
-    # Compute likelihood using the negllk function (assuming it is defined elsewhere)
+    # Compute likelihood using the negllk function
     negllk(k_val, lambda_val,
            x = trans_snp_dist,
            t = trans_time_dist,
            s = trans_sites,
            nbfitmu = nb_fit$estimate["mu"],
-           nbfitsize = nb_fit$estimate["size"])
+           nbfitsize = nb_fit$estimate["size"],
+           truncation_point=truncation_point,
+           prior_k=prior_k,
+           prior_lambda=prior_lambda)
   })
 
   # Plot the results using ggplot
