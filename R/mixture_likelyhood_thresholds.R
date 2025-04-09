66ee6c9b5acc322d19784dda10289d744a02b546
Zunair <zunairkhurram@gmail.com>
2025-04-09 17:08:25 +1000

    calculating LHRs

--- R/mixture_likelyhood_thresholds.R
+++ R/mixture_likelyhood_thresholds.R
@@ -10,31 +10,47 @@
 #' @export
 #'
 #' @examples
-mixture_likelyhood_thresholds <- function(trans_snp_dist, unrelated_snp_dist, trans_time_dist, trans_sites, year_range=c(seq(0.5, 10, 0.5))){
+mixture_likelyhood_thresholds <- function(trans_snp_dist, unrelated_snp_dist, trans_time_dist, trans_sites, truncation_point=2000){
   if ((length(trans_snp_dist) >= 30) && (length(unrelated_snp_dist) >= 30)){
-  nb_fit <- suppressWarnings(MASS::fitdistr(x=unrelated_snp_dist, densfun = "negative binomial"))
-  mix_res <- suppressWarnings(mixture_snp_cutoff(trans_snp_dist, unrelated_snp_dist, trans_time_dist, trans_sites))
+
+    #defining distribution functions for the truncated negative binomial distr, needs to be specific to the truncation point and in the global environment for fitdistplus
+    dtruncnbinom <<- function(x, mu, size){
+      dnbinom(x = x, size = size, mu = mu) / pnbinom(truncation_point, size = size, mu = mu)
+    }
+    ptruncnbinom <<- function(q, mu, size){
+      pnbinom(q = q, size = size, mu = mu) / pnbinom(truncation_point, size = size, mu = mu)
+    }
+    qtruncnbinom <<- function(p, mu, size){
+      qnbinom(p = p*(pnbinom(truncation_point, size=size, mu=mu)), size = size, mu = mu)
+    }
 
 
-    threshold <- map_dbl(year_range, function(year){
-    x <- 1:500
-    rel_prob <- mix_res$k * dpois(x, (mix_res$lambda*year*365.25*mean(trans_sites))/1000000)
-    unrel_prob <- (1-mix_res$k) * dnbinom(x, mu = nb_fit$estimate['mu'], size = nb_fit$estimate['size'])
-    thresh <- x[max(which(rel_prob > unrel_prob))]
+    # distant dataset fitting
+    m <- mean(unrelated_snp_dist)
+    v <- var(unrelated_snp_dist)
+    size <- if (v > m) {
+      m^2/(v - m)
+    }else{100}
+
+    nb_fit <- fitdistrplus::fitdist(unrelated_snp_dist, dist="truncnbinom", start=list(mu=m, size=size))
+    mix_res <- suppressWarnings(mixture_snp_cutoff(trans_snp_dist, unrelated_snp_dist, trans_time_dist, trans_sites))
 
 
+    result <-  distinct(tibble(snp_dist=trans_snp_dist, time_dist=trans_time_dist))
+    result <- result |>
+      mutate(rel_lh = (dpois(snp_dist, (mix_res$lambda*(time_dist/365.25)*mean(trans_sites)+mix_res$intercept)) / ppois(truncation_point, (mix_res$lambda*(time_dist/365.25)*mean(trans_sites)+mix_res$intercept))),
+             unrel_lh = (dnbinom(snp_dist, mu = nb_fit$estimate['mu'], size = nb_fit$estimate['size']) / pnbinom(truncation_point, mu = nb_fit$estimate['mu'], size = nb_fit$estimate['size'])))|>
+    mutate(LR = rel_lh/unrel_lh)
 
- })
-    result <- tibble(year_range, threshold, estimated_fp=NA, prop_pos=NA)
-      result$estimated_fp <- modify(result$threshold, ~{sum(unrelated_snp_dist<=.x)/length(unrelated_snp_dist)})
-      result$prop_pos <- modify(result$threshold,  ~{sum(trans_snp_dist<=.x)/length(trans_snp_dist)})
+    rm(ptruncnbinom, envir = .GlobalEnv)
+    rm(dtruncnbinom, envir = .GlobalEnv)
+    rm(qtruncnbinom, envir = .GlobalEnv)
 
-
-  return(result)
-
+    return(result)
 
   }else {
     warning("Insufficient data points to fit distributions!")
     return(tibble(NA))
   }
+
 }
